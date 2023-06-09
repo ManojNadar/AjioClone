5cb9e0f0d8187621492ebe365c42705de3dd1e22
ManojNadar <manojviru03@gmail.com>
2023-06-09 14:06:27 +0530

    ajio login

---
+++ jsFile/home.js
@@ -0,0 +1,6 @@
+let signIn = document.getElementById("sign-in");
+let loginContainer = document.getElementById("login-container");
+
+signIn.addEventListener("click", function () {
+  loginContainer.style.display = "block";
+});
